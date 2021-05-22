9d1d581ea3376b272072da2da1797ffad23cc0b7
Pae <songju19990426@gmail.com>
2021-05-22 14:58:51 +0900

    静的ファイル読み込みパスを変更

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 
 func templateHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles(
-		"views/home.html",
-		"views/header.html",
-		"views/footer.html",
+		"/app/home/views/home.html",
+		"/app/home/views/header.html",
+		"/app/home/views/footer.html",
 	)
 	if err != nil {
 		log.Fatalln("テンプレートファイルを読み込めません:", err.Error())
@@ -23,9 +23,9 @@ func templateHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	log.Println("Webサーバーを開始します...")
 	server := http.Server{
-		Addr: "127.0.0.1:8080",
+		Addr: ":8080",
 	}
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("/app/home/static"))))
 	http.HandleFunc("/home", templateHandler)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	server.ListenAndServe()
 }
