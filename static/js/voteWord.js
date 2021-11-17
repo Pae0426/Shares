ab0de3a5e99ba7eb5c83631efde20a5a2ca7b568
Pae <songju19990426@gmail.com>
2021-11-17 23:56:38 +0900

    単語いいね機能実装

--- static/js/voteWord.js
+++ static/js/voteWord.js
@@ -1,5 +1,21 @@
-function newVoteWord(id, word, empathy) {
-    return '<div class="voted-word" data-word-id=' + id + '><span class="word-text">' + word + '</span></div>'
+function newVoteWord(id, word, empathy, isEmpathy) {
+    return `
+    <div class="voted-word" data-word-id=` + id + `>
+        <span class="word-text">` + word + `</span>
+        <div class="word-empathy-container">
+            <i class="fas fa-heart word-empathy-`+ isEmpathy +`" data-word-empathy-id="` + id + `"></i>
+            <span class="word-empathy-count word-empathy-`+ isEmpathy +`">` + empathy + `</span>
+            <i class="fas fa-trash-alt trash-btn" data-trash-id="` + id + `"></i>
+        </div>
+    </div>`
+}
+
+function getWordEmpathyInfo() {
+    return $.ajax({
+        dataType: 'json',
+        type: 'GET',
+        url: '/get-word-empathy-info',
+    });
 }
 
 $('.vote-word-modal-btn').on('click', function() {
@@ -9,19 +25,77 @@ $('.vote-word-modal-btn').on('click', function() {
         type: 'GET',
         url: '/get-vote-word-info',
     }).done(function(voteWords) {
-        for(let i in voteWords) {
-            let id = voteWords[i]['id'];
-            let word = voteWords[i]['word'];
-            let empathy = voteWords[i]['empathy'];
-
-            let voteWord = newVoteWord(id, word, empathy);
-            $('.voted-word-container').append(voteWord);
-        }
+        getWordEmpathyInfo().done(function(wordEmpathyInfo) {
+            console.log(wordEmpathyInfo);
+            for(let i in voteWords) {
+                let id = voteWords[i]['id'];
+                let word = voteWords[i]['word'];
+                let empathy = voteWords[i]['empathy'];
+                let isEmpathy;
+                if(wordEmpathyInfo[i] == 1) {
+                    isEmpathy = true;
+                } else {
+                    isEmpathy = false;
+                }
+                let voteWord = newVoteWord(id, word, empathy, isEmpathy);
+                $('.voted-word-container').append(voteWord);
+            }
+            $('.vote-word-modal-item').fadeIn();
+        }).fail(function() {
+            console.log('通信失敗');
+        });
     }).fail(function() {
         console.log('通信失敗');
     });
+});
 
-    $('.vote-word-modal-item').fadeIn();
+//いいね機能
+$(document).on('click', '.word-empathy-false', function(e) {
+    $(this).addClass('word-empathy-true');
+    $(this).removeClass('word-empathy-false');
+    $(this).next('.word-empathy-count').addClass('word-empathy-true');
+    $(this).next('.word-empathy-count').removeClass('word-empathy-false');
+    let empathy_count = $(this).next('.word-empathy-count').text();
+    empathy_count = parseInt(empathy_count) + 1;
+    $(this).next('.word-empathy-count').text(empathy_count);
+
+    let id = $(this).data("word-empathy-id");
+    $.ajax({
+        dataType: 'json',
+        contentType: 'application/json',
+        type: 'POST',
+        url: '/increment-word-empathy',
+        data: JSON.stringify({
+            id: id,
+        })
+    }).done(function() {
+    }).fail(function() {
+        console.log('通信失敗');
+    });
+});
+
+$(document).on('click', '.word-empathy-true', function(e) {
+    $(this).addClass('word-empathy-false');
+    $(this).removeClass('word-empathy-true');
+    $(this).next('.word-empathy-count').addClass('word-empathy-false');
+    $(this).next('.word-empathy-count').removeClass('word-empathy-true');
+    let empathy_count = $(this).next('.word-empathy-count').text();
+    empathy_count = parseInt(empathy_count) - 1;
+    $(this).next('.word-empathy-count').text(empathy_count);
+    
+    let id = $(this).data("word-empathy-id");
+    $.ajax({
+        dataType: 'json',
+        contentType: 'application/json',
+        type: 'POST',
+        url: '/decrement-word-empathy',
+        data: JSON.stringify({
+            id: id,
+        })
+    }).done(function() {
+    }).fail(function() {
+        console.log('通信失敗');
+    });
 });
 
 $('.vote-word-modal-close-btn, .vote-word-modal-bg').on('click', function() {
--- voteWord.go
+++ voteWord.go
@@ -43,6 +43,46 @@ func getVoteWordInfo(w http.ResponseWriter, r *http.Request) {
 	w.Write(result)
 }
 
+func getWordEmpathyInfo(w http.ResponseWriter, r *http.Request) {
+	row, err := Db.Query("select max(id) from user_voted_word_" + TABLE_NAME)
+	if err != nil {
+		log.Println("エラー:", err.Error())
+	}
+	var maxId int
+	for row.Next() {
+		row.Scan(&maxId)
+	}
+
+	cookie, err := r.Cookie("user-id")
+	if err != nil {
+		log.Fatalln("エラー: ", err)
+	}
+
+	row, err = Db.Query("select word_id from user_voted_word_"+TABLE_NAME+" where user_cookie=?", cookie.Value)
+	if err != nil {
+		log.Println("エラー:", err.Error())
+	}
+	defer row.Close()
+
+	wordEmpathy := make([]int, maxId+1)
+	for row.Next() {
+		var stickyId int
+		err = row.Scan(&stickyId)
+		if err != nil {
+			log.Println("エラー", err)
+		}
+		wordEmpathy[stickyId] = 1
+	}
+
+	result, err := json.Marshal(wordEmpathy)
+	if err != nil {
+		log.Println("エラー:", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(result)
+}
+
 func voteWord(w http.ResponseWriter, r *http.Request) {
 	row, err := Db.Query("select max(id) from vote_word_info_" + TABLE_NAME)
 	if err != nil {
@@ -73,6 +113,78 @@ func voteWord(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 	}
 	sql.Exec(voteWord.Word)
+
+	res, err := json.Marshal("{200, \"ok\"}")
+	if err != nil {
+		log.Println("エラー:", err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
+
+func incrementWordEmpathy(w http.ResponseWriter, r *http.Request) {
+	type TargetId struct {
+		Id int `json:"id"`
+	}
+	var targetId TargetId
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+	if err := json.Unmarshal(body[:len], &targetId); err != nil {
+		log.Println("エラー:", err)
+	}
+
+	sql, err := Db.Prepare("update vote_word_info_" + TABLE_NAME + " set empathy=empathy+1 where id=?")
+	if err != nil {
+		log.Println("エラー:", err)
+	}
+	sql.Exec(targetId.Id)
+
+	cookie, err := r.Cookie("user-id")
+	if err != nil {
+		log.Println("エラー: ", err)
+	}
+	sql, err = Db.Prepare("insert into user_voted_word_" + TABLE_NAME + "(word_id, user_cookie) values(?, ?)")
+	if err != nil {
+		log.Println("エラー:", err)
+	}
+	sql.Exec(targetId.Id, cookie.Value)
+
+	res, err := json.Marshal("{200, \"ok\"}")
+	if err != nil {
+		log.Println("エラー:", err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
+
+func decrementWordEmpathy(w http.ResponseWriter, r *http.Request) {
+	type TargetId struct {
+		Id int `json:"id"`
+	}
+	var targetId TargetId
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+	if err := json.Unmarshal(body[:len], &targetId); err != nil {
+		log.Println("エラー:", err)
+	}
+
+	sql, err := Db.Prepare("update vote_word_info_" + TABLE_NAME + " set empathy=empathy-1 where id=?")
+	if err != nil {
+		log.Println("エラー:", err)
+	}
+	sql.Exec(targetId.Id)
+
+	cookie, err := r.Cookie("user-id")
+	if err != nil {
+		log.Println("エラー: ", err)
+	}
+	sql, err = Db.Prepare("delete from user_voted_word_" + TABLE_NAME + " where word_id=? and user_cookie=?")
+	if err != nil {
+		log.Println("エラー:", err)
+	}
+	sql.Exec(targetId.Id, cookie.Value)
 
 	res, err := json.Marshal("{200, \"ok\"}")
 	if err != nil {
